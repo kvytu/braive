26090c8f094c2c1456db831bdd758cd464f3631e
kvytu <143767248+kvytu@users.noreply.github.com>
2023-08-31 20:39:36 -0700

    Update main.go

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ var (
 )
 
 func init() {
-	// var err error
+	var err error
 
 	// Default config
 	cfg = &config{
@@ -30,7 +30,8 @@ func init() {
 	}
 
 	// Init app
-	app = portapps.NewWithCfg("brave", "Brave", cfg)
+	if app, err = portapps.NewWithCfg("brave", "Brave", cfg); err != nil {
+	}
 }
 
 func main() {
